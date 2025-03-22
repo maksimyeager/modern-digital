3ce57c15731b01f9d377ad1accd4307801eaf6f4
maksimyeager <maksimyeager@gmail.com>
2025-03-22 20:06:15 +0400

    Bag fix

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import { Button } from "../ui";
 import Link from "next/link";
 import Image from "next/image";
 import { MobileNavbar } from "./mobile-navbar";
